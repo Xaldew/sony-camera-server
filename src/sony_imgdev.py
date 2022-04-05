aac0009146d7119b9f398179cf61fca9bc067a87
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2022-04-05 23:05:53 +0200

    Simplified XML schema prefixes.

--- src/sony_imgdev.py
+++ src/sony_imgdev.py
@@ -33,62 +33,40 @@ def parse_upnp_device_definition(upnp_dd):
     root = cElementTree.XML(upnp_dd)
     root = xml2dict.XmlDictConfig(root)
 
-    # _upnp_version = root.get("{urn:schemas-upnp-org:device-1-0}specVersion", "")
-    device = root.get("{urn:schemas-upnp-org:device-1-0}device", "")
-    name = device.get("{urn:schemas-upnp-org:device-1-0}friendlyName", "")
+    upnp_prefix = "{urn:schemas-upnp-org:device-1-0}"
+    # _upnp_version = root.get(f"{upnp_prefix}specVersion", "")
+    device = root.get(f"{upnp_prefix}device", "")
+    name = device.get(f"{upnp_prefix}friendlyName", "")
 
     # Retrieve all services
-    dd_services = device.get(
-        "{urn:schemas-upnp-org:device-1-0}serviceList", "")
-    dd_serv = dd_services.get("{urn:schemas-upnp-org:device-1-0}service", "")
+    dd_services = device.get(f"{upnp_prefix}serviceList", "")
+    dd_serv = dd_services.get(f"{upnp_prefix}service", "")
 
     # Are these service definitions ever actually used?
     upnp_services = []
     for srv in dd_serv:
-        scpdurl = srv.get("{urn:schemas-upnp-org:device-1-0}SCPDURL",
-                          "")
-        control_url = srv.get("{urn:schemas-upnp-org:device-1-0}controlURL",
-                              "")
-        event_suburl = srv.get("{urn:schemas-upnp-org:device-1-0}eventSubURL",
-                               "")
-        service_id = srv.get("{urn:schemas-upnp-org:device-1-0}serviceId",
-                             "")
-        service_type = srv.get("{urn:schemas-upnp-org:device-1-0}serviceType",
-                               "")
+        scpdurl = srv.get(f"{upnp_prefix}SCPDURL", "")
+        control_url = srv.get(f"{upnp_prefix}controlURL", "")
+        event_suburl = srv.get(f"{upnp_prefix}eventSubURL", "")
+        service_id = srv.get(f"{upnp_prefix}serviceId", "")
+        service_type = srv.get(f"{upnp_prefix}serviceType", "")
         upnp_services.append(UPnPService(scpdurl, control_url, event_suburl,
                                          service_id, service_type))
 
     # Retrieve functions relevant to the Sony AV extension "ScalarWebAPI".
-    scalarweb_devinfo = device.get(
-        "{urn:schemas-sony-com:av}X_ScalarWebAPI_DeviceInfo",
-        "")
-    version = scalarweb_devinfo.get(
-        "{urn:schemas-sony-com:av}X_ScalarWebAPI_Version",
-        "")
-    imgdev = scalarweb_devinfo.get(
-        "{urn:schemas-sony-com:av}X_ScalarWebAPI_ImagingDevice",
-        "")
-    live_view = imgdev.get(
-        "{urn:schemas-sony-com:av}X_ScalarWebAPI_LiveView_URL",
-        "")
-    default_fn = imgdev.get(
-        "{urn:schemas-sony-com:av}X_ScalarWebAPI_DefaultFunction",
-        "")
-    scalarweb_servlist = scalarweb_devinfo.get(
-        "{urn:schemas-sony-com:av}X_ScalarWebAPI_ServiceList",
-        "")
-    scalarweb_servlist = scalarweb_servlist.get(
-        "{urn:schemas-sony-com:av}X_ScalarWebAPI_Service",
-        "")
+    sony_prefix = "{urn:schemas-sony-com:av}X_ScalarWebAPI"
+    scalarweb_devinfo = device.get(f"{sony_prefix}_DeviceInfo", "")
+    version = scalarweb_devinfo.get(f"{sony_prefix}_Version", "")
+    imgdev = scalarweb_devinfo.get(f"{sony_prefix}_ImagingDevice", "")
+    live_view = imgdev.get(f"{sony_prefix}_LiveView_URL", "")
+    default_fn = imgdev.get(f"{sony_prefix}_DefaultFunction", "")
+    scalarweb_service = scalarweb_devinfo.get(f"{sony_prefix}_ServiceList", "")
+    scalarweb_service = scalarweb_service.get(f"{sony_prefix}_Service", "")
 
     services = []
-    for srv in scalarweb_servlist:
-        _type = srv.get(
-            "{urn:schemas-sony-com:av}X_ScalarWebAPI_ServiceType",
-            "")
-        url = srv.get(
-            "{urn:schemas-sony-com:av}X_ScalarWebAPI_ActionList_URL",
-            "")
+    for srv in scalarweb_service:
+        _type = srv.get(f"{sony_prefix}_ServiceType", "")
+        url = srv.get(f"{sony_prefix}_ActionList_URL", "")
         services.append(ScalarWebService(_type, url))
 
     webapi = ScalarWebAPI(services, live_view, default_fn)
